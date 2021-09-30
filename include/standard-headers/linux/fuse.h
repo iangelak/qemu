b95b6e76ac20d2e0346404d8a9c3f5fae78760a9
Ioannis Angelakopoulos <iangelak@redhat.com>
2021-09-30 17:24:52 -0400

    virtiofsd: Add a fsnotify opcode and in and out message structs
    Add a new opcode for the suppport of the fsnotify subsystem in virtiofsd.
    Also add two new structs that will carry the fsnotify information between
    the guest and virtiofsd.
    
    The "in" struct, which corresponds to a fsnotify request will carry the
    mask for the events the guest wants to monitor, an identifier that is
    unique to the guest process that wants to monitor events on an inode and
    the action which corresponds to the creation, modification and deletion of
    a watch on an inode. The guest does not need to send the inode information
    along with the fsnotify request since that information is already
    encapsulated in the FUSE request.
    
    The "out" struct corresponds to the notification that virtiofsd generates
    when an event occurs and the guest has to be notified about it. This struct
    carries the information about the inode for which the event ocurred, the
    mask that was placed on the inode, the name of the file for which the event
    occured (If that is available) and the cookie. This information corresponds
    to the information that a process receives from the kernel when a event
    occurs for a monitored inode. Since now virtiofsd is monitoring inodes for
    events and not the guest kernel, it is the responsibility of virtiofsd to
    send this information to the guest.
    
    Signed-off-by: Ioannis Angelakopoulos <iangelak@redhat.com>

--- include/standard-headers/linux/fuse.h
+++ include/standard-headers/linux/fuse.h
@@ -190,6 +190,7 @@
  *  7.36
  *  - add FUSE_HAVE_FSNOTIFY
  *  - add FUSE_FSNOTIFY_<bits>
+ *  - add fuse_notify_fsnotify_(in,out)
  */
 
 #ifndef _LINUX_FUSE_H
@@ -397,6 +398,7 @@ struct fuse_file_lock {
  *			write/truncate sgid is killed only if file has group
  *			execute permission. (Same as Linux VFS behavior).
  * FUSE_SETXATTR_EXT:	Server supports extended struct fuse_setxattr_in
+ * FUSE_HAVE_FSNOTIFY:  Remote fsnotify/inotify subsystem support
  */
 #define FUSE_ASYNC_READ		(1 << 0)
 #define FUSE_POSIX_LOCKS	(1 << 1)
@@ -428,6 +430,7 @@ struct fuse_file_lock {
 #define FUSE_SUBMOUNTS		(1 << 27)
 #define FUSE_HANDLE_KILLPRIV_V2	(1 << 28)
 #define FUSE_SETXATTR_EXT	(1 << 29)
+#define FUSE_HAVE_FSNOTIFY      (1 << 30)
 
 /**
  * CUSE INIT request/reply flags
@@ -573,7 +576,11 @@ enum fuse_opcode {
 	FUSE_COPY_FILE_RANGE	= 47,
 	FUSE_SETUPMAPPING	= 48,
 	FUSE_REMOVEMAPPING	= 49,
+<<<<<<< HEAD
 	FUSE_SYNCFS		= 50,
+=======
+	FUSE_FSNOTIFY		= 51,
+>>>>>>> 91454115e4 (virtiofsd: Add a fsnotify opcode and in and out message structs)
 
 	/* CUSE specific operations */
 	CUSE_INIT		= 4096,
@@ -591,6 +598,7 @@ enum fuse_notify_code {
 	FUSE_NOTIFY_RETRIEVE = 5,
 	FUSE_NOTIFY_DELETE = 6,
 	FUSE_NOTIFY_LOCK = 7,
+	FUSE_NOTIFY_FSNOTIFY = 8,
 	FUSE_NOTIFY_CODE_MAX,
 };
 
@@ -628,6 +636,18 @@ struct fuse_getattr_in {
 	uint32_t	getattr_flags;
 	uint32_t	dummy;
 	uint64_t	fh;
+};
+
+struct fuse_notify_fsnotify_in {
+	uint32_t mask;
+        uint32_t padding;
+};
+
+struct fuse_notify_fsnotify_out {
+	uint64_t inode;
+	uint64_t mask;
+	uint32_t namelen;
+	uint32_t cookie;
 };
 
 #define FUSE_COMPAT_ATTR_OUT_SIZE 96
